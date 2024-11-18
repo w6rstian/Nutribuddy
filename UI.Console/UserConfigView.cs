793140e8bc578885464b2e54411d6d78cdd43af4
w6rstian <quarennc@gmail.com>
2024-11-18 14:17:37 +0100

    User view

---
+++ UI.Console/UserConfigView.cs
@@ -0,0 +1,32 @@
+﻿using Nutribuddy.Core.Controllers;
+using Spectre.Console;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nutribuddy.UI.Console
+{
+	internal class UserConfigView : IView
+	{
+		private readonly UserController _userController;
+		private readonly Action _navigateToMainMenu;
+		
+		public UserConfigView(Action navigateToMainMenu)
+		{
+			_navigateToMainMenu = navigateToMainMenu;
+		}
+
+		public void Show()
+		{
+			AnsiConsole.Clear();
+
+			var table = new Table();
+			table.Caption("User Data", style: null);
+			table.AddColumn("").Centered();
+			table.AddColumn("").Centered();
+
+		}
+	}
+}
