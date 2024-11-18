d5063416e3ec25ef8fc5c335fa128dce5c0fbaf9
w6rstian <quarennc@gmail.com>
2024-11-18 15:42:14 +0100

    Registered new views

--- Program.cs
+++ Program.cs
@@ -8,13 +8,17 @@ namespace Nutribuddy
     {
         static void Main(string[] args)
         {
+            var userController = new UserController();
+
 			var viewManager = new ViewManager();
 			viewManager.RegisterView("IntroSequence", new IntroSequenceView(() => viewManager.ShowView("MainMenu")));
-			viewManager.RegisterView("MainMenu", new MainMenuView());
+			viewManager.RegisterView("MainMenu", new MainMenuView(() => viewManager.ShowView("UserDetails")));
+			viewManager.RegisterView("UserDetails", new UserDetailsView(userController, () => viewManager.ShowView("MainMenu"), () => viewManager.ShowView("UserConfig")));
+			viewManager.RegisterView("UserConfig", new UserConfigView(userController, () => viewManager.ShowView("UserDetails")));
 
-			//viewManager.ShowView("IntroSequence");
-			//viewManager.ShowView("MainMenu");
-
+            //viewManager.ShowView("IntroSequence");
+            //viewManager.ShowView("MainMenu");
+            viewManager.ShowView("UserDetails");
 			var foodController = new FoodController("Data/FoodData.json");
 			var foodUI = new TestFoodView(foodController);
             //foodUI.Run();
@@ -23,7 +27,7 @@ namespace Nutribuddy
             var dishUI = new TestDishView(foodController, dishController);
             dishUI.Run();
 
-            var userController = new UserController();
+            //var userController = new UserController();
             var userUI = new TestUserView(userController);
             //userUI.Run();
         }
