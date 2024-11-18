8163437b71f5d29b421bfa1bdf83dceec33cf9b7
w6rstian <quarennc@gmail.com>
2024-11-18 21:01:56 +0100

    Registered more views

--- Program.cs
+++ Program.cs
@@ -9,27 +9,36 @@ namespace Nutribuddy
         static void Main(string[] args)
         {
             var userController = new UserController();
+			var foodController = new FoodController("Data/FoodData.json");
+			var dishController = new DishController("Data/DishData.json");
 
 			var viewManager = new ViewManager();
 			viewManager.RegisterView("IntroSequence", new IntroSequenceView(() => viewManager.ShowView("MainMenu")));
-			viewManager.RegisterView("MainMenu", new MainMenuView(() => viewManager.ShowView("UserDetails")));
-			viewManager.RegisterView("UserDetails", new UserDetailsView(userController, () => viewManager.ShowView("MainMenu"), () => viewManager.ShowView("UserConfig")));
+			viewManager.RegisterView("MainMenu", new MainMenuView(
+                () => viewManager.ShowView("UserDetails"),
+                () => viewManager.ShowView("Food"),
+                () => viewManager.ShowView("Dish")));
+			viewManager.RegisterView("UserDetails", new UserDetailsView(userController, 
+                () => viewManager.ShowView("MainMenu"), 
+                () => viewManager.ShowView("UserConfig")));
 			viewManager.RegisterView("UserConfig", new UserConfigView(userController, () => viewManager.ShowView("UserDetails")));
+            viewManager.RegisterView("Food", new FoodView(foodController, () => viewManager.ShowView("MainMenu")));
+            viewManager.RegisterView("Dish", new DishView(foodController, dishController, () => viewManager.ShowView("MainMenu")));
 
-            //viewManager.ShowView("IntroSequence");
-            viewManager.ShowView("MainMenu");
+            viewManager.ShowView("IntroSequence");
+            //viewManager.ShowView("MainMenu");
             //viewManager.ShowView("UserDetails");
-			var foodController = new FoodController("Data/FoodData.json");
-			var foodUI = new TestFoodView(foodController);
-            //foodUI.Run();
+			
+			//var foodUI = new FoodView(foodController);
+            //foodUI.Show();
 
-            var dishController = new DishController("Data/DishData.json");  
-            var dishUI = new TestDishView(foodController, dishController);
-            dishUI.Run();
+            
+            //var dishUI = new DishView(foodController, dishController);
+            //dishUI.Show();
 
             //var userController = new UserController();
-            var userUI = new TestUserView(userController);
-            //userUI.Run();
+            //var userUI = new TestUserView(userController);
+            //userUI.Show();
         }
     }
 }
