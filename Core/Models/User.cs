8fd978738f26ce9b189c12ea854d61496bd650b4
w6rstian <quarennc@gmail.com>
2024-11-18 15:39:43 +0100

    Added BMI and calories properties

--- Core/Models/User.cs
+++ Core/Models/User.cs
@@ -12,7 +12,20 @@ namespace Nutribuddy.Core.Models
         public double Height { get; set; } = 180.0;
         public int Age { get; set; } = 20;
         public string Gender { get; set; } = "Male";
+        public double BMI { get; set; }
+        public double CaloricNeeds { get; set; }
 
-
+        public User()
+        {
+            BMI = Weight / ((Height / 100) * (Height / 100));
+            if (Gender == "Male")
+            {
+				CaloricNeeds = 10 * Weight + 6.25 * Height - 5 * Age + 5; //chop
+			}
+            else
+            {
+				CaloricNeeds = 10 * Weight + 6.25 * Height - 5 * Age - 161; //baba
+			}
+		}
     }
 }
