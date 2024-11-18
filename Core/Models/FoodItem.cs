07a4b3b6f8db46c9e5b27687d4c02d4e5facca10
w6rstian <quarennc@gmail.com>
2024-11-18 20:59:17 +0100

    Warning fix

--- Core/Models/FoodItem.cs
+++ Core/Models/FoodItem.cs
@@ -8,8 +8,8 @@ namespace Nutribuddy.Core.Models
 {
     internal class FoodItem
     {
-        public string Description { get; set; }
-        public Dictionary<string, double> Nutrients { get; set; }
+        public string Description { get; set; } = String.Empty;
+        public Dictionary<string, double> Nutrients { get; set; } = new Dictionary<string, double>();
         public double QuantityInGrams { get; set; } // ilość danego skłdanika w gramach
     }
 }
