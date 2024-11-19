a2b0aa23745a4bc9dafc0376eebd666358e1b2e3
w6rstian <quarennc@gmail.com>
2024-11-19 01:37:19 +0100

    Eating history model

---
+++ Core/Models/EatHistory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nutribuddy.Core.Models
+{
+	internal class EatHistory
+	{
+		public List<(DateTime, Dish)> DishEatHistory { get; set; }
+		public List<(DateTime, FoodItem)> FoodItemEatHistory { get; set; }
+
+		public EatHistory()
+		{
+			DishEatHistory = new();
+			FoodItemEatHistory = new();
+		}
+	}
+}
