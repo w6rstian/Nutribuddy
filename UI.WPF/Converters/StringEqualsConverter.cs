eecc61888dc9a50349a34856eb86537dcb50a59e
w6rstian <quarennc@gmail.com>
2024-12-17 00:04:00 +0100

    Converter that searches for the correct button to change IsChecked

---
+++ UI.WPF/Converters/StringEqualsConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Nutribuddy.UI.WPF.Converters
+{
+    public class StringEqualsConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // Compare value with the converter parameter
+            return value != null && value.ToString() == parameter.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null; // No need for this in this case
+        }
+    }
+}
