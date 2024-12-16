9e31e63e3085476c87e49b8f900ce37856ac5778
w6rstian <quarennc@gmail.com>
2024-12-16 19:12:23 +0100

    Added WPF Views and Viewmodels

---
+++ UI.WPF/ViewModel/NavigationVM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Nutribuddy.UI.WPF.ViewModel
+{
+    class NavigationVM : ViewModelBase
+    {
+        private object _currentView;
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set { _currentView = value; OnPropertyChanged(); }
+        }
+
+        public ICommand HomeCommand { get; set; }
+    }
+}
