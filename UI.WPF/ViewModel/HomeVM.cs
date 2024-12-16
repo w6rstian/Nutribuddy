f0311ab52079cbfc5baf9d3c9fe935a13b0253f2
w6rstian <quarennc@gmail.com>
2024-12-16 23:30:54 +0100

    ProfileCommand

--- UI.WPF/ViewModel/HomeVM.cs
+++ UI.WPF/ViewModel/HomeVM.cs
@@ -1,13 +1,28 @@
-﻿using System;
+﻿using Nutribuddy.UI.WPF.View;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Nutribuddy.UI.WPF.ViewModel
 {
     class HomeVM : ViewModelBase
     {
-        public HomeVM() { }
+        private object _currentView;
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set { _currentView = value; OnPropertyChanged(); }
+        }
+
+        public ICommand ProfileCommand { get; set; }
+        private void Profile(object obj) => CurrentView = new ProfileVM();
+        public HomeVM()
+        {
+            ProfileCommand = new RelayCommand(Profile);
+
+        }
     }
 }
