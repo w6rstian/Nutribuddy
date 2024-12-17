a57f97c8cdc82a9d7cfd1b56f3f4ecc836568250
w6rstian <quarennc@gmail.com>
2024-12-17 02:25:44 +0100

    Added profile

--- UI.WPF/ViewModel/ProfileVM.cs
+++ UI.WPF/ViewModel/ProfileVM.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Nutribuddy.UI.WPF.ViewModel
 {
@@ -13,6 +14,21 @@ namespace Nutribuddy.UI.WPF.ViewModel
         {
             get { return _currentView; }
             set { _currentView = value; OnPropertyChanged(); }
+        }
+
+        // TODO: DODAC OBSLUGE RESZTY WIDOKOW JAK POWSTANA WIDOKI
+
+        public ICommand SettingsCommand { get; set; }
+
+        private void Settings(object obj)
+        {
+            var navigationVM = App.Current.MainWindow.DataContext as NavigationVM;
+            navigationVM?.SettingsCommand.Execute(null);
+        }
+
+        public ProfileVM()
+        {
+            SettingsCommand = new RelayCommand(Settings);
         }
     }
 }
