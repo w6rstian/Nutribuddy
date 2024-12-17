c403add5bc37c558f151c9b4d56d736f9ec70887
w6rstian <quarennc@gmail.com>
2024-12-17 02:25:17 +0100

    Meals

---
+++ UI.WPF/ViewModel/MealsVM.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nutribuddy.UI.WPF.ViewModel
+{
+    class MealsVM : ViewModelBase
+    {
+        private object _currentView;
+        private string _currentViewName;
+
+        public string CurrentViewName
+        {
+            get { return _currentViewName; }
+            set { _currentViewName = value; OnPropertyChanged(); }
+        }
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set { _currentView = value; OnPropertyChanged(); }
+        }
+
+        // TODO: DODAC OBSLUGE RESZTY WIDOKOW JAK POWSTANA WIDOKI
+
+        public MealsVM()
+        {
+
+        }
+    }
+}
