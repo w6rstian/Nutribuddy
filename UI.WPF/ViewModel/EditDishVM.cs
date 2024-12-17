d769b6d5ad574a56ae5dcbd91cacca4d3ac1b65f
szymanowsk <szymanowskakam@gmail.com>
2024-12-17 11:21:20 +0100

    edit dish init

---
+++ UI.WPF/ViewModel/EditDishVM.cs
@@ -0,0 +1,29 @@
+﻿using Nutribuddy.Core.Controllers;
+using Nutribuddy.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Nutribuddy.UI.WPF.ViewModel
+{
+    class EditDishVM : ViewModelBase
+    {
+        private DishController _dishController;
+        private Dish _dish;
+
+        public string DishName
+        {
+            get => _dish.Name;
+            set
+            {
+                _dish.Name = value;
+                OnPropertyChanged();
+            }
+        }
+
+    }
+}
